8716680a3cd249f39f6e05395ac78cc15a9a2512
Carlyn Lee <carlyn.lee@gmail.com>
2026-02-02 08:23:58 -0800

    Scope providers to siblings instead of nesting

--- react-display/src/App.tsx
+++ react-display/src/App.tsx
@@ -14,37 +14,42 @@ function App() {
 		<HashRouter>
 			<div className='flex flex-col h-screen w-full overflow-hidden'>
 				<TimeProvider>
-					<SponsorProvider>
-						<ScheduleProvider refreshInterval={60000}>
-							<Header />
-							<Routes>
-								<Route path='/' element={
-									<div className='flex flex-1 bg-white overflow-hidden'>
-										<div className='w-4/5 p-2 overflow-y-auto'>
-											<ScheduleCarousel
-												maxDisplay={6}
-												rotationInterval={15000}
-											/>
-										</div>
-										<div className='w-1/5 p-2'>
-											<SponsorBanner
-												displayCount={3}
-												rotationInterval={10000}
-											/>
-										</div>
-									</div>
-								} />
-								<Route path='/sponsors' element={
-									<div className='flex-1 overflow-hidden'>
-										<SponsorThankYou
-											otherSponsorsPerPage={12}
-											rotationInterval={8000}
+					<Header />
+					<Routes>
+						<Route path='/' element={
+							<div className='flex flex-1 bg-white overflow-hidden'>
+								{/* Schedule area - ScheduleProvider wraps only ScheduleCarousel */}
+								<div className='w-4/5 p-2 overflow-y-auto'>
+									<ScheduleProvider refreshInterval={60000}>
+										<ScheduleCarousel
+											maxDisplay={6}
+											rotationInterval={15000}
 										/>
-									</div>
-								} />
-							</Routes>
-						</ScheduleProvider>
-					</SponsorProvider>
+									</ScheduleProvider>
+								</div>
+								{/* Sponsor banner - SponsorProvider wraps only SponsorBanner */}
+								<div className='w-1/5 p-2'>
+									<SponsorProvider>
+										<SponsorBanner
+											displayCount={3}
+											rotationInterval={10000}
+										/>
+									</SponsorProvider>
+								</div>
+							</div>
+						} />
+						<Route path='/sponsors' element={
+							<div className='flex-1 overflow-hidden'>
+								{/* SponsorProvider wraps only SponsorThankYou */}
+								<SponsorProvider>
+									<SponsorThankYou
+										otherSponsorsPerPage={12}
+										rotationInterval={8000}
+									/>
+								</SponsorProvider>
+							</div>
+						} />
+					</Routes>
 				</TimeProvider>
 			</div>
 		</HashRouter>
